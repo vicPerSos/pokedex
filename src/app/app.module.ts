c9175138503b93b454933fae70cdd4bf9007855c
vicPerSos <vpsb888@gmail.com>
2025-03-11 06:31:31 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListComponent } from './components/list/list.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BuscadorComponent,
+    FooterComponent,
+    HeaderComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
